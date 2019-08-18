f3a9db6b9ac0ff4b19417a28d984b7e75813156a
kevin <narvaezkevin82@gmail.com>
2019-08-17 19:54:13 -0500

    segunda version con interfaz principal

--- app/src/main/java/com/example/frutimat/MainActivity.java
+++ app/src/main/java/com/example/frutimat/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.frutimat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText et_nombre;
+    private ImageView iv_personaje;
+    private TextView tv_bestScore;
+    private MediaPlayer mp;
 
+    int num_aleatorio=(int)Math.random()*10;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        et_nombre = findViewById(R.id.txt_nombre);
+        iv_personaje = findViewById(R.id.imagenview_Personaje);
+        tv_bestScore = findViewById(R.id.textView_BestScore);
+
+        //COLOCAR EL ICONO DENTRO DEL ACTION BAR
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.mipmap.ic_launcher);
+
+        int id;
+
     }
 }
